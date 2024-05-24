807b0bb2fc20d357215a53c7ff90f139b33ee5d3
Brian Han <shan790@aucklanduni.ac.nz>
2024-05-24 17:12:00 +1200

    Finished functionality for loading and making the graph

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -1,3 +1,69 @@
 package nz.ac.auckland.se281;
 
-public class Country {}
+public class Country {
+
+  private String name;
+  private String continent;
+  private int taxRate;
+
+  public Country(String name, String continent, int taxRate) {
+    this.name = name;
+    this.continent = continent;
+    this.taxRate = taxRate;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getContinent() {
+    return continent;
+  }
+
+  public void setContinent(String continent) {
+    this.continent = continent;
+  }
+
+  public int getTaxRate() {
+    return taxRate;
+  }
+
+  public void setTaxRate(int taxRate) {
+    this.taxRate = taxRate;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((continent == null) ? 0 : continent.hashCode());
+    result = prime * result + taxRate;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Country other = (Country) obj;
+    if (name == null) {
+      if (other.name != null) return false;
+    } else if (!name.equals(other.name)) return false;
+    if (continent == null) {
+      if (other.continent != null) return false;
+    } else if (!continent.equals(other.continent)) return false;
+    if (taxRate != other.taxRate) return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Country [name=" + name + ", continent=" + continent + ", taxRate=" + taxRate + "]";
+  }
+}
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,21 +1,45 @@
 package nz.ac.auckland.se281;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** This class is the main entry point. */
 public class MapEngine {
 
+  private Map<String, Country> countryMap;
+  private Graph<Country> worldGraph;
+
   public MapEngine() {
-    // add other code here if you want
+    // HashSet is ideal for checking if an element is in it
+    countryMap = new HashMap<>();
+    worldGraph = new Graph<>();
 
     loadMap(); // keep this mehtod invocation
   }
 
   /** invoked one time only when constracting the MapEngine class. */
   private void loadMap() {
+    // Read countries and adjacencies from provided files
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
-    System.out.println((countries.get(0)).split(",")[0]);
+
+    // Make country vertexes
+    for (String countryEntry : countries) {
+      String[] country = countryEntry.split(",");
+      Country newCountry = new Country(country[0], country[1], Integer.parseInt(country[2]));
+      countryMap.put(country[0], newCountry);
+    }
+
+    for (String adjacency : adjacencies) {
+      String[] countriesNames = adjacency.split(",");
+
+      String rootCountry = countriesNames[0];
+
+      for (int i = 1; i < countriesNames.length; i++) {
+        worldGraph.addEdge(countryMap.get(rootCountry), countryMap.get(countriesNames[i]));
+      }
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
